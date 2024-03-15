537459c1e3a79c85b5361545a3742f63962c1b50
Ossian Eriksson <ossiane7@gmail.com>
2024-03-15 13:13:37 +0100

    Update system.h

--- src/system.h
+++ src/system.h
@@ -1,1 +1,10 @@
 #pragma once
+
+class CardSystem // for card handling  (give take and shuffle)
+{};
+
+class PointSystem // for calculating points 
+{};
+
+class GameSystem // For text user interface
+{};
