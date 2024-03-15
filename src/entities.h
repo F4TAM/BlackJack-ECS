c46b21982f6028f95c73e004747afd5d11c6f949
Ossian Eriksson <ossiane7@gmail.com>
2024-03-15 13:09:41 +0100

    added comments

--- src/entities.h
+++ src/entities.h
@@ -1,31 +1,31 @@
-#pragma once
-#include <unordered_map>
-#include <string>
-#include <typeindex>
+#pragma once // Include guard to prevent multiple inclusion
+#include <unordered_map> // Include necessary library for unordered_map
+#include <string> // Include necessary library for string
+#include <typeindex> // Include necessary library for type_index
 
-
-
-
-class Entity {
+class Entity { // Define a class named Entity
 private:
-    int id;
-    std::unordered_map<std::type_index, void*> components;
+    int id; // Member variable to store entity ID
+    std::unordered_map<std::type_index, void*> components; // Map to store components of different types
 
 public:
+    // Constructor to initialize the entity with an ID
     Entity(int id) : id(id) {}
 
+    // Method to add a component to the entity
     template <typename T>
     void addComponent(T* component) {
+        // Store the component in the map using its type as the key
         components[std::type_index(typeid(T))] = static_cast<void*>(component);
     }
 
+    // Method to get a component of a specific type from the entity
     template <typename T>
     T* getComponent() {
-        auto it = components.find(std::type_index(typeid(T)));
-        if (it != components.end()) {
-            return static_cast<T*>(it->second);
+        auto it = components.find(std::type_index(typeid(T))); // Find the component in the map
+        if (it != components.end()) { // If component is found
+            return static_cast<T*>(it->second); // Cast and return the component pointer
         }
-        return nullptr;
+        return nullptr; // Return nullptr if component is not found
     }
 };
-
