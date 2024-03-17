bd0c8e84f63ff11290e1a3c89d4398b21d796f00
Ossian Eriksson <91731229+ossiandev@users.noreply.github.com>
2024-03-17 14:21:00 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,30 @@
 #include <iostream>
 #include <string>
 
+
 int main()
 {
-    
+    Entity player;
+    Entity dealer;
+    player.addComponent(new InputComponent());
+    dealer.addComponent(new InputComponent());
+    player.addComponent(new ScoreComponent());
+    dealer.addComponent(new ScoreComponent());
+    player.addComponent(new HandComponent());
+    dealer.addComponent(new HandComponent());
+
+    bool gameRunning = true;
+
+    std::string clearConsole = "\033[2J\033[1;1H";
+    std::string startUI = "Welcome to Blackjack! Press 's' to start the game or 'q' to quit: \n";
+
+    while (gameRunning)
+    {
+        std::cout << clearConsole;
+        std::cout << startUI;
+
+    }
+
+
+
 }
